dd49d8c2f881f45e713db448dc8cfd25a2aabb03
Mohith Morees <mohithmorees1124@gmail.com>
2025-03-03 22:25:09 +0530

    New java program

---
+++ Marks.java
@@ -0,0 +1,9 @@
+class Marks {
+public static void main(String[] args){
+int s1=10,s2=22,s3=30;
+int total =s1+s2+s3;
+double maxMarks = 30;
+double percentage = ((double) (total/maxMarks)*100);
+System.out.println(percentage);
+}
+}
