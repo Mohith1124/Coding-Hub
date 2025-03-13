fbdc7457b508169c365952ead93c11af62690a6f
Mohith Morees <mohithmorees1124@gmail.com>
2025-03-13 13:01:38 +0530

    Java Programs

---
+++ Tables.java
@@ -0,0 +1,15 @@
+class Tables
+{
+  public static void main(String[] args)
+  {  
+   for(int i=1;i<=10;i++)
+  {
+    System.out.println("Table "+i+" is :");
+    for(int j=1;j<=10;j++)
+    {
+      System.out.println(i+" X "+j+" = "+i*j);
+    }
+ } 
+    
+}
+}
