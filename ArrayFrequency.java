7af7fc18b14a99fc863b7aa91bad904b60449743
Mohith Morees <mohithmorees1124@gmail.com>
2025-04-09 22:31:46 +0530

    Java Programs

--- ArrayFrequency.java
+++ ArrayFrequency.java
@@ -1,45 +1,12 @@
-/*
-class ArrayFrequency
-{
- public static void main(String[] args)
- {
-  int a[] = {1,2,3,2,1,4,5,6,7,6,5,4,8,9,0,6,4};
-  for(int i=0;i<a.length;i++) 
-  {
-   boolean isCounted = false;
-   for(int k=0;k<i;k++) 
-   {
-    if(a[i]==a[k]) 
-    {
-     isCounted = true;
-     break;
-    }
-   }
-    if(isCounted)
-        continue ;
-    int count = 0;
-    for(int j=0;j<a.length;j++)
-    {       
-      if(a[i]==a[j])
-      {
-        count++;
-        boolean isCount = true;
-      }
-    }
-     System.out.println("Frequency of "+a[i]+" is :"+count);
-   }
-  }
-}
-  */
 import java.util.Arrays;
 class ArrayFrequency
 {
  public static void main(String[] args)
  {
-  int a[] = {1,2,2,3,3,3,4,4};
+  int a[] = {5,4,3,2,4,5,6,7,8,9,7,0};
   int n = a.length;
-  Arrays.sort(a);
   int count = 1;
+  Arrays.sort(a);
   for(int i=1;i<a.length;i++)
   {
     if(a[i]==a[i-1])
@@ -48,10 +15,11 @@ class ArrayFrequency
     }
     else
     {
-      System.out.println("Frequency of "+a[i-1]+" is :"+count); 
+      System.out.println("Frequency of "+a[i-1]+" is :"+count);   
+      count = 1;
     }
    }
     System.out.println("Frequency of "+a[n-1]+" is :"+count);
    }
 }
-    
+   
